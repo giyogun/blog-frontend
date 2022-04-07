e4a75eb85ad40392e990525b49e89ef382fc6109
Godwin Iyogun <giyogun@gmail.com>
2022-04-07 20:27:59 +0100

    update backendUrl

--- src/components/assets/backendUrl.js
+++ src/components/assets/backendUrl.js
@@ -1,4 +1,4 @@
-const url = "https://infotainment-hub.herokuapp.com" + "/api";
+const url = "https://infotainment-hub.herokuapp.com/api";
 // const url = "http://localhost:5000/api";
 
 export default url;
