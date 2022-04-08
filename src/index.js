064b2ef34fc4c556b0afff6b9443b39a4b68b32b
Godwin Iyogun <giyogun@gmail.com>
2022-04-08 01:47:59 +0100

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 import { PostsProvider } from "./context/postsContext";
 
 ReactDOM.render(
   // <React.StrictMode>
-    <BrowserRouter>
-      <PostsProvider>
-        <App />
-      </PostsProvider>
-    </BrowserRouter>,
+  <HashRouter>
+    <PostsProvider>
+      <App />
+    </PostsProvider>
+  </HashRouter>,
   // </React.StrictMode>,
   document.getElementById("root")
 );
